678402b2e96c680ce3d86288d0ffd7a7cc1c39d3
EagerBeaver98 <jooke98@outlook.com>
2020-10-26 21:22:09 +0000

    Functions within functions

--- head.js
+++ head.js
@@ -6,6 +6,10 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const head = function(array) {
+  return array[0]
+}
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));

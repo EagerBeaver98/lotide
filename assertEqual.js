3dab1c0c6a906bb75b930eccd2e98ba72a9d6fec
EagerBeaver98 <jooke98@outlook.com>
2020-10-26 20:58:45 +0000

    Testing matches

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertation Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertation Failed: " + actual + " !== " + expected);
+  }
+};
+
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);

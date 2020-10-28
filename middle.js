3290606e07f6ab7d5853c5decc9001c66b3cc6df
EagerBeaver98 <jooke98@outlook.com>
2020-10-28 19:42:58 +0000

    Pulls middle value of array

---
+++ middle.js
@@ -0,0 +1,41 @@
+const eqArrays = function(array1, array2) {
+  let answer;
+  for (let x = 0; x < array1.length && x < array2.length; x++) {
+    if (array1[x] !== array2[x]) {
+      return false;
+    } else {
+      answer = true;
+    }
+  }
+  return answer;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`Assertation Passed: ${array1} === ${array2}`);
+  } else {
+    console.log(`Assertaion Failed: ${array1} !== ${array2}`);
+  }
+};
+
+assertArraysEqual;
+
+const middle = function(array) {
+  if (array.length % 2 !== 0 && array.length > 1) {
+    for (let x = 0; array.length > 1; x++) {
+      array.pop();
+      array.shift();
+    }
+  } else if (array.length % 2 === 0 && array.length > 2) {
+    for (let x = 0; array.length > 2; x++) {
+      array.pop();
+      array.shift();
+    }
+  } else if (array.length <= 2) {
+    return [];
+  }
+  return array;
+  
+};
+
+console.log(middle([1]));

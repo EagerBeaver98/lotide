5fc208f21e5039bf3c537ec250e815f788696447
EagerBeaver98 <jooke98@outlook.com>
2020-10-28 21:27:31 +0000

    Add letter totals

---
+++ countLetters.js
@@ -0,0 +1,23 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertation Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertation Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const answer = {};
+  for (const letter of sentence) {
+    if (answer[letter]) {
+      answer[letter] += 1;
+    } else if (letter === ' ') {
+      let x = 1;
+    } else {
+      answer[letter] = 1;
+    }
+  }
+  return answer;
+};
+
+console.log(countLetters('lighthouse in the house'));

733dbf4c3f27c9a9fa357d754ee63c514d7c1a70
EagerBeaver98 <jooke98@outlook.com>
2020-11-02 17:26:38 +0000

    Refactoring using module calls

--- head.js
+++ head.js
@@ -1,15 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertation Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertation Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const head = function(array) {
   return array[0];
 };
 
 
-console.log(assertEqual(head([5,6,7]), 5));
-console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+module.exports = head;
